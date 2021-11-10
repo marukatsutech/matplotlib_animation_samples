71ea27f2fb2c3eda1d87e41a55ded4d2096e4222
marukatsutech <marukatsutech@gmail.com>
2021-11-10 19:38:58 +0800

    First commit

---
+++ sample1_step2.py
@@ -0,0 +1,18 @@
+# Sample1 step2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+def update(f):
+    y = np.sin(x - f)
+    ax1.plot(x, y)
+
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.grid()
+x = np.linspace(0, 10, 100)
+
+anim = animation.FuncAnimation(fig, update, interval=100)
+plt.show()
